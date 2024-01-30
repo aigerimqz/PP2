fa87686e0ec88754dc699ee1ff3d6446a1d6446f
Aigerim <ai_manat@kbtu.kz>
2024-01-30 23:45:37 +0600

    Added lab2 files

---
+++ lab2/booleans.py
@@ -0,0 +1,20 @@
+#ex1
+print(10>9)
+#Answer is True
+
+#ex2
+print(10 == 9)
+#Answer is False
+
+
+#ex3
+print(10 < 9)
+#Answer is False
+
+#ex4
+print(bool("abc"))
+#Answer is True
+
+#ex5
+print(bool(0))
+#Answer is False
---
+++ lab2/dictionaries.py
@@ -0,0 +1,48 @@
+# Dictionaries are used to store data values in key:value pairs.
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+#ex1
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(car.get("model"))
+
+
+#ex2
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car["year"] = 2020
+print(car)
+
+#ex3
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car["color"] = "red"
+print(car)
+
+
+#ex4
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car.pop("model")
+print(car)
+
+
+#ex5
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+car.clear()
+print(car)
